758e87fb199e574f856eb505d6e3485b70cfef47
guilhermereboucas-dotcom <guilherme.reboucas@academico.ifpb.edu.br>
2026-05-08 15:49:02 -0300

    terceiro commit

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,6 +5,8 @@ import EstadosConcretos.EstadoAberto;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("------PASSO 1------");
+        System.out.println("Criando primeiro chamado...");
         Chamado chamado = new Chamado("XRBR", "NACIONAL", "Jorge", new EstadoAberto());
         System.out.println("------PASSO 2------");
         chamado.resolver();
